3e3f518b6fb6974659484f42a0c04b1c3036d75a
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-12-08 18:47:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+            // Get the select element
+            var selectElement = document.getElementById("colorSelect");
+
+            // Get the selected index
+            var selectedIndex = selectElement.selectedIndex;
+
+            // Remove the selected option
+            selectElement.remove(selectedIndex);
+        }
